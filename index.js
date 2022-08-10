23580536a4063353edbcbf4e5ffae9a734180d61
Tim Krashevsky <tim.krashevsky@gmail.com>
2022-08-10 14:33:21 -0500

    added eraser, color selection and size selection

--- index.js
+++ index.js
@@ -5,13 +5,45 @@ container.addEventListener('click', toggleStart)
 let sizeSelector = document.querySelector('.size input');
 sizeSelector.addEventListener('input', changeSize);
 
-let colorSelector = document.querySelectorAll('.colors');
-let eraserSelector = document.querySelector('.eraser');
+let colorSelector = Array.from(document.querySelectorAll('.color-container > *'));
+colorSelector.forEach(elem => {
+    if (elem.className === "default" || elem.className === "rainbow") {
+        elem.addEventListener('click', changeGridColor);
+    } else if (elem.className === "choose") {
+        let userChoose = elem.querySelector('input');
+        ['click', 'input'].forEach(evt => {
+            userChoose.addEventListener(evt, changeGridColor, false);
+        })
+    }
+});
+let currentGradient = "default";
+let lastChosenColor = 0;
 
+let eraserSelector = document.querySelector('.eraser');
+eraserSelector.addEventListener('click', clearBoard)
+
+function clearBoard() {
+    // Clear colors from existing children
+    let children = container.children;
+    for (let i = 0; i < children.length; i++) {
+        let currChild = children[i];
+        currChild.style.backgroundColor = null;
+    }
+}
+
+function changeGridColor(e) {
+    let currElem = e.target;
+
+    if (currElem.className === "default" || currElem.className === "rainbow") {
+        currentGradient = currElem.className;
+        return;
+    }
+
+    currentGradient = currElem.value;
+}
 
 function changeSize(e) {
     let slideBar = e.target;
-    console.log(slideBar.nextElementSibling)
 
     // Change next sibling element output to reflect size and also change grid size
     slideBar.nextElementSibling.textContent = slideBar.value + "x" + slideBar.value;
@@ -28,7 +60,6 @@ function removeAllChildren(node) {
 function toggleStart(e) {
     // Parent node
     let gridContainer = e.target.parentElement;
-    console.log(gridContainer);
 
     // Add hover listener to each div
     let gridChildren = gridContainer.children;
@@ -43,6 +74,9 @@ function generateGrid(n) {
     // Set the repeat display grid to be nxn
     container.style.gridTemplateRows = `repeat(${n}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+
+    // Remove children
+    removeAllChildren(container);
 
     // Start with 16x16 div containers
     for (let row = 1; row <= n; row++) {
@@ -67,9 +101,15 @@ function generateGrid(n) {
 
 function toggleColor(e) {
     let selectedElem = e.target;
-    console.log(selectedElem)
 
-    selectedElem.style.backgroundColor = "black";
+    if (currentGradient === "default") {
+        selectedElem.style.backgroundColor = "black";
+    } else if (currentGradient === "rainbow") {
+        selectedElem.style.backgroundColor = `hsl(${lastChosenColor}, 98%, 51%)`;
+        lastChosenColor = (lastChosenColor + 25) % 360;
+    } else {
+        selectedElem.style.backgroundColor = currentGradient; 
+    }
 }
 
 generateGrid(16);
